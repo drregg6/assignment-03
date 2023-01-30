548bfb872fb0d20ce4084efc1ba662dbc6999eda
drregg6 <dave.r.regg@gmail.com>
2023-01-30 16:31:20 -0500

    Elements added with styles

--- assignment-03/ViewController.swift
+++ assignment-03/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amBackgroundImage: UIImageView!
+    @IBOutlet weak var pmBackgroundImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        amBackgroundImage.isHidden = true
     }
 
 
