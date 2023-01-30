467cc82d218cd26b68ecfa83670dee9a19ad41ff
drregg6 <dave.r.regg@gmail.com>
2023-01-30 17:39:22 -0500

    Time is tickin' away

--- assignment-03/ViewController.swift
+++ assignment-03/ViewController.swift
@@ -9,15 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeRemainingLabel: UILabel!
     @IBOutlet weak var amBackgroundImage: UIImageView!
     @IBOutlet weak var pmBackgroundImage: UIImageView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    var timer = Timer()
+    var formatter = DateFormatter()
+    var date = Date()
+    var hour: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        formatter.dateFormat = "E, d MMM yyyy HH:mm:ss"
+        dateLabel.text = formatter.string(from: date)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(self.tick) , userInfo: nil, repeats: true)
         amBackgroundImage.isHidden = true
     }
 
-
+    @IBAction func buttonPress(_ sender: UIButton) {
+        amBackgroundImage.isHidden = !amBackgroundImage.isHidden
+        pmBackgroundImage.isHidden = !pmBackgroundImage.isHidden
+    }
+    
+    @objc func tick() {
+        date = Date()
+        formatter.dateFormat = "E, d MMM yyyy HH:mm:ss"
+        
+        dateLabel.text = formatter.string(from: date)
+    }
+    
+    func setBackground(_ hour: Int) {
+        
+    }
+    
 }
 
