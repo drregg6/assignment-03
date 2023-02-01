7bb613e335bb74ffe7f02e3e0facd63e09a91255
drregg6 <dave.r.regg@gmail.com>
2023-02-01 17:35:01 -0500

    Audio plays at the end of the timer

--- assignment-03/ViewController.swift
+++ assignment-03/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -21,6 +22,10 @@ class ViewController: UIViewController {
     var timer = Timer()
     var formatter = DateFormatter()
     var date = Date()
+    var audioPlayer: AVAudioPlayer! = AVAudioPlayer()
+    let chime = Bundle.main.path(forResource: "chime", ofType: "mp3")
+
+    // bool for timer validation? - that can track button
     var isTimerOn: Bool = false
     
     override func viewDidLoad() {
@@ -32,6 +37,13 @@ class ViewController: UIViewController {
         timeRemainingLabel.isHidden = true
         timerButton.setTitle("Start Timer", for: .normal)
         setBackground()
+        
+        // Load audio file
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: chime!) as URL)
+        } catch {
+            // Error
+        }
     }
 
     @IBAction func buttonPress(_ sender: UIButton) {
@@ -47,6 +59,9 @@ class ViewController: UIViewController {
         
         // Ensure that User cannot interact with Picker after button press
         datePicker.isUserInteractionEnabled = false
+        
+        timerButton.setTitle("Stop Music", for: .normal)
+        timerButton.isUserInteractionEnabled = false
     }
     
     @objc func tick() {
@@ -57,9 +72,13 @@ class ViewController: UIViewController {
     }
     
     @objc func startCountdown() {
-        if timeLeft! >= 0 {
+        if timeLeft! > 0 {
             timeRemainingLabel.text = "Time Remaining: " + stringFromTimeInterval(timeLeft: timeLeft!)
             timeLeft! -= 1
+        } else if timeLeft! == 0 {
+            timeLeft! -= 1
+            // Sound chime
+            audioPlayer.play()
         } else {
             // Invalid timer
             timer.invalidate()
